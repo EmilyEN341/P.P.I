02c9c7e6bde506ff58c89f60c4ac32416e220b29
Emily Elisabeth Neves <emily.en@aluno.ifsc.edu.br>
2023-10-02 10:20:40 -0300

    att

---
+++ Aula/JavaScript/Aulas/a1/a1.js
@@ -0,0 +1,17 @@
+console.log("Hello");
+/*variáveis var, const, let;*/
+var a = 42;
+var b = 27;
+
+var c = a+b;
+
+console.log(c);
+if (a>7) {
+    let d = 34;
+    console.log(typeof(d));
+
+    d = "Blah"
+    console.log(typeof(d));
+    console.log("a é maior que 7");
+}
+/*console.log(d);  Não funciona fora do bloco onde a variável foi "setada"*/
