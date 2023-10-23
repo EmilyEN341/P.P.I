d0ad9c06769e85fadef3926f8c7f4be08f36eb40
Emily Elisabeth Neves <emily.en@aluno.ifsc.edu.br>
2023-10-23 10:54:35 -0300

    Upd ex4 js1

--- Listas/Lista JS 01/ex4/main.js
+++ Listas/Lista JS 01/ex4/main.js
@@ -2,11 +2,7 @@
 let contador = document.getElementById("contador");
 let valor = 0;
 
-let p1 = document.getElementById("p1");
-let p2 = document.getElementById("p2");
-
-
 function contar(){
-    contador.innerHTML = valor.toString();
+    contador.innerHTML = valor;
     valor ++;
 }
